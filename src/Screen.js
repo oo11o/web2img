bccac430cf45d8f97622c32f7dd2204d185e2324
Vlad Imir <ymyr.vlad@gmail.com>
2022-08-30 14:53:19 +0300

    feat: add an option to take screenshot by selector

--- src/Screen.js
+++ src/Screen.js
@@ -9,29 +9,49 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
     });
 };
 const puppeteer = require('puppeteer');
-const getPage = (url, img = null) => __awaiter(void 0, void 0, void 0, function* () {
-    const browser = yield puppeteer.launch();
-    const page = yield browser.newPage();
-    try {
-        yield page.goto(url);
-    }
-    catch (e) {
-        yield browser.close();
-        throw new Error(e.message);
-    }
-    yield page.screenshot({ path: 'example.png' });
-    yield browser.close();
-});
 module.exports = class Screen {
+    constructor() {
+        this.conf = {
+            image: {
+                web: 'web.png',
+                selector: 'selector.png'
+            }
+        };
+    }
     capture(url, selector = null) {
         return __awaiter(this, void 0, void 0, function* () {
+            const browser = yield puppeteer.launch({ headless: true });
+            const page = yield browser.newPage();
+            yield page.setViewport({
+                width: 1920,
+                height: 1080,
+                deviceScaleFactor: 1,
+            });
             try {
-                yield getPage(url);
-                return true;
+                yield page.goto(url);
             }
             catch (e) {
+                yield browser.close();
                 throw new Error(e.message);
             }
+            if (selector) {
+                try {
+                    yield page.waitForSelector(selector, { timeout: 2000 }); // Method to ensure that the element is loaded
+                    const section = yield page.$(selector);
+                    yield section.screenshot({
+                        path: this.conf.image.selector
+                    });
+                    yield browser.close();
+                    return this.conf.image.selector;
+                }
+                catch (e) {
+                    yield browser.close();
+                    throw new Error(`Can't find selector \n ${e.message}`);
+                }
+            }
+            yield page.screenshot({ path: this.conf.image.web });
+            yield browser.close();
+            return this.conf.image.web;
         });
     }
 };
--- src/Screen.ts
+++ src/Screen.ts
@@ -1,27 +1,47 @@
 const puppeteer = require('puppeteer');
 
-const getPage = async (url: string, img: string | null = null) => {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    try {
-        await page.goto(url);
-    } catch (e : any) {
-        await browser.close()
-        throw new Error(e.message);
-    }
-
-    await page.screenshot({path: 'example.png'});
-    await browser.close();
-}
-
 module.exports = class Screen {
-
-    async capture(url: string, selector: string | null = null) {
-        try {
-            await getPage(url);
-            return true;
-        }catch (e: any) {
-            throw new Error(e.message);
+    conf = {
+        image: {
+            web: 'web.png',
+            selector: 'selector.png'
         }
     }
+
+    async capture(url: string, selector: string | null = null) {
+        const browser = await puppeteer.launch({headless: true});
+        const page = await browser.newPage();
+        await page.setViewport({
+            width:  1920,
+            height: 1080,
+            deviceScaleFactor: 1,
+        });
+
+        try {
+            await page.goto(url);
+        } catch (e: any) {
+            await browser.close()
+            throw new Error(e.message);
+        }
+
+        if (selector) {
+            try {
+                await page.waitForSelector(selector, {timeout: 2000});          // Method to ensure that the element is loaded
+                const section = await page.$(selector);
+                await section.screenshot({
+                    path: this.conf.image.selector
+                })
+                 await browser.close();
+                return this.conf.image.selector;
+            } catch (e: any){
+                await browser.close();
+                throw new Error(`Can't find selector \n ${e.message}`);
+            }
+        }
+
+        await page.screenshot({path: this.conf.image.web});
+        await browser.close();
+        return this.conf.image.web;
+    }
+
 }
